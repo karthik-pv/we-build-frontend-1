0656e1e43abd130baf0210ee0f658bf360f74e41
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-01-23 16:58:59 +0530

    student me page integrated

--- src/screens/Me.js
+++ src/screens/Me.js
@@ -1,49 +1,64 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Me.css'
 import Header from '../components/Header'
+import useApiRequest from '../hooks/useApiRequest'
+import { STUDENT_INFO } from '../api/student'
+import AuthContext from '../context/AuthContext'
 
 const Dashboard = () => {
+
+    const { authToken } = useContext(AuthContext)
+
+    const { fetch, data, error, loading } = useApiRequest({
+        method: 'get',
+        url: STUDENT_INFO,
+        headers: {
+            token: authToken
+        }
+    })
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
+    console.log(data, error, loading)
+
     return (
         <>
             <Header />
             <div id='me'>
                 <div id='me_header'>
                     <span id='me_header_greeting'>Hi there,</span>
-                    <span id='me_header_name'>Tisheel N Bashyam</span>
+                    <span id='me_header_name'>{data?.name}</span>
                 </div>
                 <div id='me_details'>
                     <table id='me_details_table'>
                         <tbody>
                             <tr>
                                 <th>USN</th>
-                                <td>1RN22CS170</td>
+                                <td>{data?.usn}</td>
                             </tr>
                             <tr>
                                 <th>Email</th>
-                                <td>1rn22cs170.tisheelnbashyam@rnsit.ac.in</td>
+                                <td>{data?.email}</td>
                             </tr>
                             <tr>
                                 <th>Phone</th>
-                                <td>+91 8073279428</td>
+                                <td>{data?.phone}</td>
                             </tr>
                             <tr>
                                 <th>Branch</th>
-                                <td>CSE</td>
+                                <td>{data?.branch}</td>
                             </tr>
                             <tr>
                                 <th>Semister</th>
-                                <td>3</td>
+                                <td>{data?.sem}</td>
                             </tr>
                             <tr>
                                 <th>Section</th>
-                                <td>C</td>
+                                <td>{data?.section}</td>
                             </tr>
-                            <tr>
-                                <th>Subjects</th>
-                                <td>
-                                    BIDTK108/208, BIDTK108/208
-                                </td>
-                            </tr>
+                            {/* subjects */}
                         </tbody>
                     </table>
                     <div id='me_details_graph'>
