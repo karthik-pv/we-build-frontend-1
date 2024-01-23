34087ac40d94be51e7686cc82183ffe2057b4af2
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-01-23 17:14:59 +0530

    Header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import './Header.css'
 import { Link, useLocation } from 'react-router-dom'
+import { GoArrowRight } from "react-icons/go"
 
 const Header = () => {
     return (
-        <div id='header'>
-            <span id='header_title'>College Test Portal</span>
-            <div id='header_navigation'>
-                <ul id='header_navigation_list'>
+        <div className='flex flex-row justify-between items-center p-2'>
+            <span className='text-black font-extrabold text-4xl'>College Test Portal</span>
+            <div>
+                <ul className='flex gap-10 font-extrabold text-base'>
                     <li>
                         <Link to='/'>Me</Link>
                     </li>
@@ -22,7 +22,9 @@ const Header = () => {
                     </li>
                 </ul>
             </div>
-            <button id='logout_btn'>Log out</button>
+            <button className='flex items-center gap-1 text-red-600 font-extrabold'>
+                logout<GoArrowRight />
+            </button>
         </div>
     )
 }
