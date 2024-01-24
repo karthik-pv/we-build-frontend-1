a4e1c5353863b510a3f9feabaf43f7e3aa93aa43
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-01-24 18:43:10 +0530

    minor changes

--- src/components/TestCard.jsx
+++ src/components/TestCard.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 const TestCard = () => {
     return (
-        <div className='bg-white flex flex-col gap-5 p-2 m-4 shadow rounded-lg'>
+        <div className='bg-white flex flex-col gap-5 p-2 m-4 shadow rounded-lg w-80'>
             <div className='flex flex-col'>
+                <span className='font-extrabold'>#6389h231r9bn8t6nbr</span>
                 <span className='font-extrabold'>Principles Of Programming</span>
                 <span className='font-extrabold'>BPOP203</span>
-                <span className='font-extrabold'>#6389</span>
             </div>
             <div className='flex justify-between items-end'>
                 <table className='text-left'>
                     <tbody>
                         <tr>
                             <th>Date</th>
-                            <td>23 11 2023</td>
+                            <td className='px-2'>23 11 2023</td>
                         </tr>
                         <tr>
                             <th>Start</th>
-                            <td>11:30</td>
+                            <td className='px-2'>11:30</td>
                         </tr>
                         <tr>
                             <th>End</th>
-                            <td>12:30</td>
+                            <td className='px-2'>12:30</td>
                         </tr>
                     </tbody>
                 </table>
