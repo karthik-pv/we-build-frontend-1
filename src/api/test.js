9a31a2403b4f6963592799d7d40f7bd5982001d5
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-01-25 23:02:16 +0530

    tests fetching

---
+++ src/api/test.js
@@ -0,0 +1,3 @@
+const TEST_HOST = 'http://127.0.0.1:7003/api/v1/test'
+
+export const TEST_GET = TEST_HOST + '/'
--- src/screens/Tests.jsx
+++ src/screens/Tests.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import TestCard from '../components/TestCard'
 import Header from '../components/Header'
+import useApiRequest from '../hooks/useApiRequest'
+import AuthContext from '../context/AuthContext'
+import { TEST_GET } from '../api/test'
 
 const Tests = () => {
+
+    const { authToken } = useContext(AuthContext)
+
+    const { fetch, data, error, loading } = useApiRequest({
+        method: 'get',
+        url: TEST_GET,
+        headers: {
+            token: authToken
+        },
+        body: {}
+    })
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
     return (
         <>
             <Header />
@@ -11,11 +30,17 @@ const Tests = () => {
                     <span className='text-black font-extrabold text-3xl'>#Tests</span>
                     <input className='outline-none shadow rounded-lg p-2 w-80' type='text' placeholder='search' />
                 </div>
+                {
+                    loading && <span className='text-black font-extrabold text-3xl'>Loading...</span>
+                }
+                {
+                    error && <span>Somthing went wrong</span>
+                }
                 <div className='flex flex-wrap'>
                     <TestCard />
                     <TestCard />
                 </div>
-            </div>
+            </div >
         </>
     )
 }
