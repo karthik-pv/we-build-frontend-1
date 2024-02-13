ac0a707ce3fc13ef919f6f1cd65bda2c2988971e
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-02-13 22:58:36 +0530

    fetch student

--- src/screens/Me.jsx
+++ src/screens/Me.jsx
@@ -1,27 +1,55 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Header from '../components/Header'
 import useApiRequest from '../hooks/useApiRequest'
 import { STUDENT_INFO } from '../api/student'
 import AuthContext from '../context/AuthContext'
 import { RiLoader4Line } from "react-icons/ri"
+import axios from 'axios'
+import { SUBJECT_FILTER } from '../api/subject'
 
 const Dashboard = () => {
 
+    const [student, setStudent] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
     const { authToken } = useContext(AuthContext)
 
-    const { fetch, data, error, loading } = useApiRequest({
-        method: 'get',
-        url: STUDENT_INFO,
-        headers: {
-            token: authToken
+    const fetchStudent = async () => {
+
+        setLoading(true)
+
+        try {
+
+            const student = await axios.get(STUDENT_INFO, {
+                headers: {
+                    token: authToken
+                }
+            })
+
+            const subjects = await axios.post(SUBJECT_FILTER, { _id: student.data.subjects })
+
+            student.data.subjects = subjects.data
+
+            setStudent(student.data)
+
+        } catch (error) {
+
+            setError(error)
+
+        } finally {
+
+            setLoading(false)
+
         }
-    })
+
+    }
 
     useEffect(() => {
-        fetch()
-    }, [])
 
-    // TODO get subject details
+        fetchStudent()
+
+    }, [])
 
     return (
         <>
@@ -41,34 +69,34 @@ const Dashboard = () => {
                         <div className='flex flex-col p-10 gap-10'>
                             <div className='flex flex-col'>
                                 <span className='text-black text-5xl font-extrabold'>Hi there,</span>
-                                <span className='text-black text-2xl font-extrabold'>{data?.name}</span>
+                                <span className='text-black text-2xl font-extrabold'>{student?.name}</span>
                             </div>
                             <div className='flex gap-10'>
                                 <table className='bg-white text-left shadow rounded-lg'>
                                     <tbody>
                                         <tr>
                                             <th className='p-2'>USN</th>
-                                            <td className='p-2'>{data?.usn}</td>
+                                            <td className='p-2'>{student?.usn}</td>
                                         </tr>
                                         <tr>
                                             <th className='p-2'>Email</th>
-                                            <td className='p-2'>{data?.email}</td>
+                                            <td className='p-2'>{student?.email}</td>
                                         </tr>
                                         <tr>
                                             <th className='p-2'>Phone</th>
-                                            <td className='p-2'>{data?.phone}</td>
+                                            <td className='p-2'>{student?.phone}</td>
                                         </tr>
                                         <tr>
                                             <th className='p-2'>Branch</th>
-                                            <td className='p-2'>{data?.branch}</td>
+                                            <td className='p-2'>{student?.branch}</td>
                                         </tr>
                                         <tr>
                                             <th className='p-2'>Semister</th>
-                                            <td className='p-2'>{data?.sem}</td>
+                                            <td className='p-2'>{student?.sem}</td>
                                         </tr>
                                         <tr>
                                             <th className='p-2'>Section</th>
-                                            <td className='p-2'>{data?.section}</td>
+                                            <td className='p-2'>{student?.section}</td>
                                         </tr>
                                         <tr>
                                             <th className='p-2'>Subjects</th>
@@ -76,9 +104,9 @@ const Dashboard = () => {
                                                 <table>
                                                     <tbody>
                                                         {
-                                                            data?.subjects.map((subject, index) => {
+                                                            student?.subjects?.map((subject, index) => {
                                                                 return <tr key={index}>
-                                                                    <td className='py-1'>{subject}</td>
+                                                                    <td className='py-1'>{subject?.subjectCode}</td>
                                                                 </tr>
                                                             })
                                                         }
